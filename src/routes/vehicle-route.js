0de43d819862cbb8cb2b8076b1bcd8236c317081
Maia <vincecake008@gmail.com>
2024-10-10 09:00:14 -0300

    init: first commit

---
+++ src/routes/vehicle-route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  store,
+  index,
+  show,
+  update,
+  destroy,
+} from "../controllers/vehicle-controller.js";
+
+const router = Router();
+
+router.post("/", store);
+router.get("/", index);
+router.get("/:id", show);
+router.put("/:id", update);
+router.delete("/:id", destroy);
+
+export default router;
